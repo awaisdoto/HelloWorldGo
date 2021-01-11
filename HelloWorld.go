bf65f536e49da741f1fc31b503ac0c4323f1804b
Awais Saleem <awaissaleem442@gmail.com>
2021-01-11 17:47:24 +0500

    Add files via upload

---
+++ HelloWorld.go
@@ -0,0 +1,8 @@
+// This is a daily challenge for LHD Build 2021 
+// Hello World in a new Language 
+package main
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World! Welcome to Local Hack Day Build 2021 :DDDD")
+}
